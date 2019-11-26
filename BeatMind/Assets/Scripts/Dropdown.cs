4d0bd210c2b06f7eb57a9504952e3fe15bb260af
tigreacuatico <gbachs@edu.tecnocampus.cat>
2019-11-26 23:49:47 +0100

    no message

---
+++ BeatMind/Assets/Scripts/Dropdown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropdown : MonoBehaviour
+{
+    public Dropdown dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PopulateList()
+    {
+        List<string> names = new List<string> { "EASY", "MEDIUM", "HARD" };
+        
+    }
+}
