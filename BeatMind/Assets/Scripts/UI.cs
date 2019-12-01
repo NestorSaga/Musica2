59b5b52cfbfc86dff225eba31b19c3543520c736
tigreacuatico <gbachs@edu.tecnocampus.cat>
2019-12-02 00:20:26 +0100

    ui

--- BeatMind/Assets/Scripts/UI.cs
+++ BeatMind/Assets/Scripts/UI.cs
@@ -9,12 +9,14 @@ public class UI : MonoBehaviour
     public GameObject m_player1VictoryText;
     public GameObject m_player2VictoryText;
 
-
+    public Text m_player1FinalScoreText;
+    public Text m_player2FinalScoreText;
 
     void Start()
     {
         m_player1VictoryText.SetActive(false);
         m_player2VictoryText.SetActive(false);
+
     }
 
     
@@ -29,5 +31,10 @@ public class UI : MonoBehaviour
     public void EnablePlayer2TextAsWinner()
     {
         m_player1VictoryText.SetActive(false);
+    }
+    public void DisplayFinalScore(int scorePlayer1, int scorePlayer2)
+    {
+        m_player1VictoryText.GetComponent<Text>().text = "Player 1: "+ scorePlayer1.ToString();
+        m_player2VictoryText.GetComponent<Text>().text = "Player 2: " + scorePlayer1.ToString();
     }
 }
