17711febcaf287b69b524ca91ce708a68681cad9
albertmc36 <albertmecu@gmail.com>
2019-12-02 03:38:58 +0100

    Musiquita

---
+++ BeatMind/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+        audioManager.Play("Starting Menu Music");
+    }
+
+    void Update()
+    {
+        
+    }
+}
